727b0a4b1573771ae237d6b3125c7c98a1e02ca5
Brisu <brisu@alumni.ubc.ca>
2020-03-16 16:26:40 -0700

    Broke chat_createChannel
    Unknown C is not a valid character error
    Layout for project create is finished.

--- app/src/main/java/com/example/cosc341project/project_create.java
+++ app/src/main/java/com/example/cosc341project/project_create.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.view.View;
 
 import static androidx.appcompat.app.AppCompatDelegate.setDefaultNightMode;
 
@@ -12,8 +13,12 @@ public class project_create extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.project_screen);
+        setContentView(R.layout.project_create);
         setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
+    }
+
+    public void createProj(View view) {
 
     }
 }
