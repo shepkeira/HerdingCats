aadd294284ab57d9738e98aea5858ab46b601154
Brisu <brisu@alumni.ubc.ca>
2020-03-16 16:33:38 -0700

    Fixed previous commit
    Violated file naming conventions with capital letters in the filename.

--- app/src/main/java/com/example/cosc341project/chat_createChannel.java
+++ app/src/main/java/com/example/cosc341project/chat_createChannel.java
@@ -9,6 +9,6 @@ public class chat_createChannel extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.chat_createChannel);
+        setContentView(R.layout.chat_create_channel);
     }
 }
--- app/src/main/java/com/example/cosc341project/chat_dM.java
+++ app/src/main/java/com/example/cosc341project/chat_dM.java
@@ -9,6 +9,6 @@ public class chat_dM extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.chat_dM);
+        setContentView(R.layout.chat_dm);
     }
 }
