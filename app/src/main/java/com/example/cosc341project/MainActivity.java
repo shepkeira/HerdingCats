35a6604cb200cea689eedad22013a3e303a57705
Brisu <brisu@alumni.ubc.ca>
2020-03-16 15:34:37 -0700

    Basic layout
    Basic layouts are created with some functioning intent calls. Main page has core functions completed.

--- app/src/main/java/com/example/cosc341project/MainActivity.java
+++ app/src/main/java/com/example/cosc341project/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.cosc341project;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import static androidx.appcompat.app.AppCompatDelegate.setDefaultNightMode;
 
@@ -14,6 +18,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        ImageButton chat = findViewById(R.id.chatBtn);
+        ImageButton task = findViewById(R.id.taskBtn);
+        ImageButton calendar = findViewById(R.id.calendarBtn);
 
+
+
+
+    }
+
+    public void openChat(View view) {
+        Intent intent = new Intent(this, chat_main.class);
+
+        startActivity(intent);
+        finish();
+    }
+
+    public void openTask(View view) {
+        Intent intent = new Intent(this, task_main.class);
+
+        startActivity(intent);
+        finish();
+    }
+
+    public void openCalendar(View view) {
+        Intent intent = new Intent(this, calendar_main.class);
+
+        startActivity(intent);
+        finish();
     }
 }
