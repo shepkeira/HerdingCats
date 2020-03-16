3b9237aa6e3f6f1e4b215ca7fa1d2cb76da97d0d
Brisu <brisu@alumni.ubc.ca>
2020-03-16 14:51:45 -0700

    Night Mode
    Night mode is now force enabled through out the app.

--- app/src/main/java/com/example/cosc341project/MainActivity.java
+++ app/src/main/java/com/example/cosc341project/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.cosc341project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+
+import static androidx.appcompat.app.AppCompatDelegate.setDefaultNightMode;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
     }
 }
