fec12eab0136de7f11a5d9ce601e387f5a6c8694
shepkeira <shepherdkeira@gmail.com>
2020-03-26 12:34:10 -0700

    Add chat window things

--- app/src/main/java/com/example/cosc341project/chat_main.java
+++ app/src/main/java/com/example/cosc341project/chat_main.java
@@ -1,6 +1,9 @@
 package com.example.cosc341project;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,9 +12,61 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import java.util.*;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class chat_main extends AppCompatActivity {
 
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+    private NavigationView nv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chat_main);
+
+        dl = (DrawerLayout)findViewById(R.id.chat_main);
+        t = new ActionBarDrawerToggle(this, dl,R.string.Open, R.string.Close);
+
+        dl.addDrawerListener(t);
+        t.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        nv = (NavigationView)findViewById(R.id.nv);
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch(id)
+                {
+                    case R.id.DM1:
+                        Toast.makeText(chat_main.this, "My Account",Toast.LENGTH_SHORT).show();break;
+                    default:
+                        return true;
+                }
+
+
+                return true;
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(t.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
+    }
+
+/*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,7 +93,7 @@ public class chat_main extends AppCompatActivity {
     }
 
    public void createChat(View view) {
-
+        //TODO
     }
 
     @Override
@@ -81,5 +136,6 @@ public class chat_main extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+*/
 
 }
